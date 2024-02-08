fec6f54de5e12026a53f27ec9dfffbd7d063c4aa
antjost <antoinejost.belmonte@gmail.com>
2024-02-08 16:17:53 +0000

    revert to previous
    git-svn-id: https://svn.onera.fr/fast/Apps/PModules@716 dfd9554f-6098-45a0-973d-6f923e442290

--- FastS/FastS/Compute/src_term.for
+++ FastS/FastS/Compute/src_term.for
@@ -236,7 +236,7 @@ c**   Source term IBC a la funk ordre 0
       if(param_int(IBC).eq.1) then 
 
         call ibcsource(ndom,param_int,ind_rhs,
-     &                 rop, cellN_IBC, coe, drodm, param_real,x,y,z)
+     &                 rop, cellN_IBC, coe, drodm)
       endif
 
 
