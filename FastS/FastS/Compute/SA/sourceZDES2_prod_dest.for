795e5e1b1b2587a7f6f877a67c824f23ae46a31c
antjost <antoinejost.belmonte@gmail.com>
2024-02-26 09:23:41 +0000

    ZDES: missing file in previous commit
    git-svn-id: https://svn.onera.fr/fast/Apps/PModules@718 dfd9554f-6098-45a0-973d-6f923e442290

--- FastS/FastS/Compute/SA/sourceZDES2_prod_dest.for
+++ FastS/FastS/Compute/SA/sourceZDES2_prod_dest.for
@@ -1,47 +1,27 @@
-#include  "FastS/Compute/mulam.for" 
+!!  amulam :: μ kinematic viscosity based on Sutherland
+#include  "FastS/Compute/mulam.for"
+!! anutild :: ν turbulent SA nu tilde  || chi:: ν/v(kinematic viscosity)
 #include  "FastS/Compute/SA/chi.for"
           fvv1 = fv1(chi)
-#include  "FastS/Compute/SA/xmut.for" 
-          ad1      =dlng(l)
-          adcut    =max(ad1 , 1.e-27)
+#include  "FastS/Compute/SA/xmut.for"
+!!ad1 :: d_w wall distance        
+          ad1      = max(dlng(l) , 1.e-27)
+          
           ra       =(xmut(l)/rop(l ,1))
-     1                  /(SA_CKARM*SA_CKARM*adcut*adcut*auijuij**0.5)
-          variable2=512.*ra*ra*ra
-          variable2=min(variable2,15.)
-          fa       =1. - tanh(variable2)
-          testfa   =0.5+ sign(.5, 0.8-fa)
-          adelta1  =testfa*sph2 +(1.-testfa)*adelta1                    ! echelle caractéristique maillage
+     1                  /(SA_CKARM*SA_CKARM*ad1*ad1*auijuij**0.5)  ! r_d = (\{nu}_t+\{nu})/(\sqrt{U_{i,j}^2}*\kappa^2*d_w^2)
+          variable2=512.*ra*ra*ra                                      ! (8*r_d)^3
+          variable2=min(variable2,15.)                                 ! cut tanh to 1
+          fa       =1. - tanh(variable2)                               ! f_d = 1-tanh((8*r_d)^3)
+          testfa   =0.5+ sign(.5, 0.8-fa)                              ! if fd>fd0: 0.5+-0.5=0 || if fd<fd0: 0.5+0.5=1
+          adelta1  =testfa*sph2 +(1.-testfa)*adelta1                   ! echelle caractéristique maillage
 
           !choix distance
-          adtild1  =ad1-fa*max(ad1-SA_RCTEDES*adelta1,0.)
-
-          dist     =max(adtild1,1.e-27)
+          adtild1  =ad1-fa*max(ad1-SA_RCTEDES*adelta1,0.)              !d_{DES}^{II}=d_w - f_d*max(d_w-C_{DES}\Delta_{DES}^{II},0)
+          dist     =max(adtild1,1.e-27)                                ! make sure we do not divide by 0
 
           !CALCUL DU TERME DE PRODUCTION
           f2       = fv2(chi,fvv1)
-
-          stild    = rot + (anutild*f2)/(SA_CKARM*SA_CKARM*dist*dist)
-
-          !!Correction SA-R
-          stild = stild + SWITCH_SA_ROT_CORR
-     &       *SA_CROT*min(0.00000000000000000001,St-rot) 
-
-          prod     = rop(l,1)*SA_CB1*stild*anutild
-
-          !CALCUL DU TERME DE DESTRUCTION
-          stild     = max(stild,0.00000000000000000001)
-          r         = anutild/(stild*SA_CKARM*SA_CKARM*dist*dist)
-          r         = min(r,10.)
-          r5        = (r*r*r*r*r-1.)
-          g         = r *(1.+SA_CW2*r5)
-          !!Correction SA-LRE
-          SA_CW2_LRE = SA_CW4 + SA_CW5 / ( (1.+ chi/40.)**2) 
-          g  = SWITCH_SA_LOW_RE*r*(1.+SA_CW2_LRE*r5) +
-     &               (1.-SWITCH_SA_LOW_RE)*g
+#include  "FastS/Compute/SA/zdes2_zdes3_common.for"
           fwg       = fw(g)
+#include  "FastS/Compute/SA/zdes2_zdes3_common_prt2.for"
 
-          destruc   =rop(l,1)*cw1*fwg*(anutild/dist)*(anutild/dist)
-
-          !CALCUL DU TERME SOURCE GLOBAL
-
-          tsource   = (prod+anvisc-destruc)
