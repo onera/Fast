64f6a666b6483ffe7b4add8728b9bc2777513689
alexandre-suss <alexandre.suss@onera.fr>
2024-11-28 15:15:40 +0100

    Modif creatPrimVars: ajout Nada

--- Fast/FastC/FastC/PyTree.py
+++ Fast/FastC/FastC/PyTree.py
@@ -269,7 +269,7 @@ def _createPrimVars(t, omp_mode, rmConsVars=True, Adjoint=False, gradP=False, is
                   fields2compact.append('centers:S'+str(vars_s[i]))
                vars.append(fields2compact)
 
-            if model_loc == 'Euler' or model_loc == 'NSLaminar' or model_loc == 'NSTurbulent':
+            if model_loc == 'Nada' or model_loc == 'Euler' or model_loc == 'NSLaminar' or model_loc == 'NSTurbulent':
               timelevel = ['', '_M1','_P1']
             elif model_loc == 'LBMLaminar':
               if lbmAJ:  timelevel = ['', '_M1']
